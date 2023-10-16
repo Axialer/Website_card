6bbd51a23ff2089b8677d1eeb77bd8f7e5e9c2d5
Axialer <capas2010@yandex.com>
2023-10-16 21:56:40 +0300

    NewVersion
    One page

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
 let current = 0;
 
+
 function showSlide(n) {
   const slides = document.getElementsByClassName('magic');
   console.log(slides);
@@ -11,19 +12,58 @@ function showSlide(n) {
 
 document.addEventListener('keydown', function(event) {
   const slides = document.getElementsByClassName('magic');
-
-  console.log(event);
   if (event.code == 'ArrowUp') {
-    console.log("1p");
     showSlide(current - 1 >= 0 ? current - 1 : slides.length - 1);
   }
   if (event.code == 'ArrowDown') {
     showSlide(current + 1 < slides.length ? current + 1 : 0);
   }
   if (event.code == 'Enter') {
-    window.location.href='projects.html';
+    if(document.getElementById('project').classList.contains('active')){ 
+      /*window.location.href='projects.html';*/
+      
+      $(Block_1).animate({marginLeft: "8%"}, 500);
+      /*$(Block_2).show();*/
+      $(Block_2).animate({opacity: 'show'}, 500);
+      $(Block_3).animate({opacity: 'hide'}, 500);
+      $(Block_4).animate({opacity: 'hide'}, 500);
+      $(Block_5).animate({opacity: 'hide'}, 500);
+      
+      /*document.getElementById('Block_1').style.marginLeft='3%';*/
+      }
+    if(document.getElementById('content').classList.contains('active')){ 
+      $(Block_1).animate({marginLeft: "8%"}, 500);
+
+      $(Block_2).animate({opacity: 'hide'}, 500);
+      $(Block_3).animate({opacity: 'show'}, 500);
+      $(Block_4).animate({opacity: 'hide'}, 500);
+      $(Block_5).animate({opacity: 'hide'}, 500);
+      }
+    if(document.getElementById('info').classList.contains('active')){ 
+      $(Block_1).animate({marginLeft: "8%"}, 500);
+
+      $(Block_2).animate({opacity: 'hide'}, 500);
+      $(Block_3).animate({opacity: 'hide'}, 500);
+      $(Block_4).animate({opacity: 'show'}, 500);
+      $(Block_5).animate({opacity: 'hide'}, 500);
+      }
+    if(document.getElementById('options').classList.contains('active')){ 
+      $(Block_1).animate({marginLeft: "8%"}, 500);
+
+      $(Block_2).animate({opacity: 'hide'}, 500);
+      $(Block_3).animate({opacity: 'hide'}, 500);
+      $(Block_4).animate({opacity: 'hide'}, 500);
+      $(Block_5).animate({opacity: 'show'}, 500);
+    }
+  
   }
   if (event.code == 'Escape') {
-    window.location.href='index.html';
+    /*window.location.href='index.html';*/
+    $(Block_1).animate({marginLeft: "30%"}, 500);
+    
+    $(Block_2).animate({opacity: 'hide'}, 500);
+    $(Block_3).animate({opacity: 'hide'}, 500);
+    $(Block_4).animate({opacity: 'hide'}, 500);
+    $(Block_5).animate({opacity: 'hide'}, 500);
   }
 });
